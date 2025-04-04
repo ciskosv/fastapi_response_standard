b4e9f86119c26603ca8423c0bda86592b2ca6140
Stefan Rivera <cisko.sv@gmail.com>
2025-04-03 19:43:11 -0600

    Fix: corrige la ruta a version.py en setup

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 from setuptools import setup, find_packages
+import os
 
-# Leer la versión desde version.py
+# Leer la versión de version.py
 version = {}
-with open("fastapi_response_standard/version.py") as f:
+version_file = ("version.py")
+with open(version_file, "r") as f:
     exec(f.read(), version)
 
 setup(
     name="fastapi_response_standard",
     version=version["__version__"],
-    description="Estándar de manejo de respuestas y errores para FastAPI",
+    description="Estándar reutilizable para manejo de errores y respuestas en FastAPI",
     author="Stefan Rivera",
     author_email="info@stefanrivera.com",
+    url="https://github.com/ciskosv/fastapi_response_standard",
     packages=find_packages(),
     install_requires=[
-        "fastapi>=0.100",
+        "fastapi>=0.95",
         "starlette>=0.27",
         "python-jose[cryptography]>=3.3.0"
     ],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
+        "License :: OSI Approved :: MIT License",
+        "Framework :: FastAPI",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
